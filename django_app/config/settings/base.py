76cb3b7c2be01243a8270b031bf8d3ad2c15b627
hongdonghyun <hong4367@gmail.com>
2017-07-04 12:26:12 +0900

    docker탑승

--- django_app/config/settings/base.py
+++ django_app/config/settings/base.py
@@ -26,6 +26,7 @@ CONFIG_SECRET_DEPLOY_FILE = os.path.join(CONFIG_SECRET_DIR, 'settings_deploy.jso
 
 CONFIG_SECRET_COMMON = json.loads(open(CONFIG_SECRET_COMMON_FILE).read())
 
+SECRET_KEY = CONFIG_SECRET_COMMON["django"]["secret_key"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -77,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -87,7 +87,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -107,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -121,11 +119,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_DIR =os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS =[
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
     STATIC_DIR,
 ]
