96478075036a67f73de121519c45c0131749fa20
hongdonghyun <hong4367@gmail.com>
2017-07-05 12:23:12 +0900

    gitignore 추가 collectstatic실행 하도록 docker추가

--- django_app/config/settings/debug.py
+++ django_app/config/settings/debug.py
@@ -5,7 +5,7 @@ config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
 WSGI_APPLICATION = 'config.wsgi.debug.application'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 DEBUG = True
 ALLOWED_HOSTS = config_secret_debug["django"]["allowed_hosts"]
 
--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -5,7 +5,7 @@ config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
 WSGI_APPLICATION = 'config.wsgi.deploy.application'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 DEBUG = True
 ALLOWED_HOSTS = config_secret_deploy["django"]["allowed_hosts"]
 
