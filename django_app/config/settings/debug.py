be90d3b96b04e2d6177e3253c3705e54829e4c0c
hongdonghyun <hong4367@gmail.com>
2017-07-05 12:12:07 +0900

    deploy json에 database추가

--- django_app/config/settings/debug.py
+++ django_app/config/settings/debug.py
@@ -9,5 +9,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 DEBUG = True
 ALLOWED_HOSTS = config_secret_debug["django"]["allowed_hosts"]
 
+# db
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 print('@@@@@@ DEBUG:', DEBUG)
 print('@@@@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -6,8 +6,11 @@ WSGI_APPLICATION = 'config.wsgi.deploy.application'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = config_secret_deploy["django"]["allowed_hosts"]
+
+# db
+DATABASES = config_secret_deploy["django"]["databases"]
 
 print('@@@@@@ DEBUG:', DEBUG)
 print('@@@@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
